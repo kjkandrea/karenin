2705ea27afc6908c4f6d4458319af84f49c6797f
jongkeun-kim <kjkandrea@gmail.com>
2022-01-08 13:05:09 +0900

    Add pingPong

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 import { hello } from './say/index.js'
+import readline from 'readline';
 
 hello()
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What do you think of Karenin? ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
