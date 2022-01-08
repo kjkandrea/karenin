67fac63d31f2e40cc0145357702c13b5a2ac60a4
jongkeun-kim <kjkandrea@gmail.com>
2022-01-08 14:42:50 +0900

    Refactor name update

--- core/config.js
+++ core/config.js
@@ -3,7 +3,7 @@ const require = createRequire(import.meta.url);
 import fs from 'fs';
 export const config = require('./config.json');
 
-export const updateName = newName => {
-  config.name = newName
+export const update = (keyName, payload) => {
+  config[keyName] = payload
   return fs.promises.writeFile('./core/config.json', JSON.stringify(config))
 }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {config, updateName } from './core/config.js'
+import {config, update } from './core/config.js'
 import { say, answer } from './api/log.js'
 import readline from 'readline';
 import chalk from 'chalk';
@@ -22,8 +22,14 @@ const recur = () => rl.question(chalk.blue(`${config.name} Waiting your command 
       break;
     // Updates
     case /^config name \w/.test(command):
+      const name = config.name
       const newName = command.replace('config name ', '')
-      await updateName(newName)
+      try {
+        await update('name', newName)
+        answer(`Now ${name} name is ${newName}.`)
+      } catch (err) {
+        console.error(err)
+      }
       break;
   }
   recur()
